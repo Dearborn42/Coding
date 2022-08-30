904e3de450f284c3282b5609082a4c6531e1ec17
AndrewMurphy <amurf26@outlook.com>
2022-08-29 17:16:33 -0700

    123

--- Main.js
+++ Main.js
@@ -5,3 +5,44 @@ function Trex(){
   document.getElementById("anything").innerHTML = "";
   document.getElementById("Trex").remove();
 }
+
+function Velociraptor(){
+  document.getElementById("heading1").innerHTML = "Velociraptor";
+  document.getElementById("anything").innerHTML = "";
+  document.getElementById("Velociraptor").remove();
+}
+
+function Pterodactyl(){
+  document.getElementById("heading1").innerHTML = "Pterodactyl";
+  document.getElementById("anything").innerHTML = "";
+  document.getElementById("Pterodactyl").remove();
+}
+
+function Spinosaurus(){
+  document.getElementById("heading1").innerHTML = "Spinosaurus";
+  document.getElementById("anything").innerHTML = "";
+  document.getElementById("Spinosaurus").remove();
+}
+
+function Triceratops(){
+  document.getElementById("heading1").innerHTML = "Triceratops";
+  document.getElementById("anything").innerHTML = "";
+  document.getElementById("Triceratops").remove();
+}
+
+function Ankylosaurus(){
+  document.getElementById("heading1").innerHTML = "Ankylosaurus";
+  document.getElementById("anything").innerHTML = "";
+  document.getElementById("Ankylosaurus").remove();
+}
+
+function Brontosaurus(){
+  document.getElementById("heading1").innerHTML = "Brontosaurus";
+  document.getElementById("anything").innerHTML = "";
+  document.getElementById("Brontosaurus").remove();
+}
+function Ichthyosaur(){
+  document.getElementById("heading1").innerHTML = "Ichthyosaur";
+  document.getElementById("anything").innerHTML = "";
+  document.getElementById("Ichthyosaur").remove();
+}
