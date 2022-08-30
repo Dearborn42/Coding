4cdaec36a07d143c752c5d2d011e1d9aef7f93f7
AndrewMurphy <amurf26@outlook.com>
2022-08-29 17:10:38 -0700

    Js

--- Main.js
+++ Main.js
@@ -1,2 +1,7 @@
 let dinos = ["T-rex", "Velocirapter", "Pterodactyl", "Spinosarous", "Tri-ceratops", "Ankylosaurus", "Brontosarus", "Ichthyosaur"];
 
+function Trex(){
+  document.getElementById("heading1").innerHTML = "T-rex";
+  document.getElementById("anything").innerHTML = "";
+  document.getElementById("Trex").remove();
+}
