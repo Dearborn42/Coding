28f31804e2abcb32135510fe885fd25cd97e5314
AndrewMurphy <amurf26@outlook.com>
2022-08-29 18:17:22 -0700

    Html

--- Main.js
+++ Main.js
@@ -4,40 +4,56 @@ let dinos = ["Tyrannosaurus rex", "Velociraptor", "Pterodactyl", "Spinosaurus", 
 function Trex(){
   document.getElementById("heading1").innerHTML = "T-rex";
   document.getElementById("anything").innerHTML = "";
+  document.getElementById("img").src = "trex.png";
+  document.getElementById("img").alt = "T-rex";
 }
 
 function Velociraptor(){
   document.getElementById("heading1").innerHTML = "Velociraptor";
   document.getElementById("anything").innerHTML = "";
+  document.getElementById("img").src = "velociraptor.png";
+  document.getElementById("img").alt = "velociraptor";
 }
 
 function Pterodactyl(){
   document.getElementById("heading1").innerHTML = "Pterodactyl";
   document.getElementById("anything").innerHTML = "";
+  document.getElementById("img").src = "pterodactyl.png";
+  document.getElementById("img").alt = "pterodactyl";
 }
 
 function Spinosaurus(){
   document.getElementById("heading1").innerHTML = "Spinosaurus";
   document.getElementById("anything").innerHTML = "";
+  document.getElementById("img").src = "spinosaurus.png";
+  document.getElementById("img").alt = "spinosaurus";
 }
 
 function Triceratops(){
   document.getElementById("heading1").innerHTML = "Triceratops";
   document.getElementById("anything").innerHTML = "";
+  document.getElementById("img").src = "triceratops.png";
+  document.getElementById("img").alt = "triceratops";
 }
 
 function Ankylosaurus(){
   document.getElementById("heading1").innerHTML = "Ankylosaurus";
   document.getElementById("anything").innerHTML = "";
+  document.getElementById("img").src = "ankylosaurus.png";
+  document.getElementById("img").alt = "ankylosaurus";
 }
 
 function Brontosaurus(){
   document.getElementById("heading1").innerHTML = "Brontosaurus";
   document.getElementById("anything").innerHTML = "";
+  document.getElementById("img").src = "brontosaurus.jpg";
+  document.getElementById("img").alt = "brontosaurus";
 }
 function Ichthyosaur(){
   document.getElementById("heading1").innerHTML = "Ichthyosaur";
   document.getElementById("anything").innerHTML = "";
+  document.getElementById("img").src = "ichthyosaur.png";
+  document.getElementById("img").alt = "ichthyosaur";
 }
 function Home(){
   document.getElementById("heading1").innerHTML = "History";
