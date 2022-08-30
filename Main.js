afc0d3869034b0e6180a8e58d6faa00fb1ef1dfa
AndrewMurphy <amurf26@outlook.com>
2022-08-29 17:53:35 -0700

    123

--- Main.js
+++ Main.js
@@ -1,53 +1,46 @@
 let dinos = ["Tyrannosaurus rex", "Velociraptor", "Pterodactyl", "Spinosaurus", "Triceratops", "Ankylosaurus", "Brontosaurus", "Ichthyosaur"];
 
+
+
 function Trex(){
   document.getElementById("heading1").innerHTML = "T-rex";
   document.getElementById("anything").innerHTML = "";
-  document.getElementById("Trex").remove();
 }
 
 function Velociraptor(){
   document.getElementById("heading1").innerHTML = "Velociraptor";
   document.getElementById("anything").innerHTML = "";
-  document.getElementById("Velociraptor").remove();
 }
 
 function Pterodactyl(){
   document.getElementById("heading1").innerHTML = "Pterodactyl";
   document.getElementById("anything").innerHTML = "";
-  document.getElementById("Pterodactyl").remove();
 }
 
 function Spinosaurus(){
   document.getElementById("heading1").innerHTML = "Spinosaurus";
   document.getElementById("anything").innerHTML = "";
-  document.getElementById("Spinosaurus").remove();
 }
 
 function Triceratops(){
   document.getElementById("heading1").innerHTML = "Triceratops";
   document.getElementById("anything").innerHTML = "";
-  document.getElementById("Triceratops").remove();
 }
 
 function Ankylosaurus(){
   document.getElementById("heading1").innerHTML = "Ankylosaurus";
   document.getElementById("anything").innerHTML = "";
-  document.getElementById("Ankylosaurus").remove();
 }
 
 function Brontosaurus(){
   document.getElementById("heading1").innerHTML = "Brontosaurus";
   document.getElementById("anything").innerHTML = "";
-  document.getElementById("Brontosaurus").remove();
 }
 function Ichthyosaur(){
   document.getElementById("heading1").innerHTML = "Ichthyosaur";
   document.getElementById("anything").innerHTML = "";
-  document.getElementById("Ichthyosaur").remove();
 }
 function Home(){
   document.getElementById("heading1").innerHTML = "History";
   document.getElementById("anything").innerHTML = "Dinosaurs are one of the greatest known reptiles of all time. They lived about 230 mya (million years ago) during a period known as the Mesozoic Era. Dinosaurs, during their time and still in history are some of the scariest and most dangerous animals. Dinosaurws were on Earth for about 245 million years until being killed by, what paletologists suspect, was a highly destructive astroid.";
-  document.getElementById("Home").remove();
 }
