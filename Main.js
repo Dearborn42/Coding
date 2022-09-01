19b6c4740a29aab18ea806c4ba997d6beb457143
AndrewMurphy <amurf26@outlook.com>
2022-08-31 17:08:22 -0700

    Update Main.js

--- Main.js
+++ Main.js
@@ -5,53 +5,53 @@ function Trex(){
   document.getElementById("heading1").innerHTML = "T-rex";
   document.getElementById("img").src = "trex.png";
   document.getElementById("img").alt = "T-rex";
-  document.getElementById("anything").innerHTML = "T-rexes are likely the most famous dinosaurs. While they were alive, they were the apex predator on land. Tyrannosauruses were amazing consumers and had about 60 teeth, and they could consume up to 500 lbs of food at one time. Today, almost everyone knows what a T-rex is and what it looks like.";
+  document.getElementById("anything").innerHTML = "T-rexes are likely the most famous dinosaurs. While they were alive, they were the apex predator on land. <br>Tyrannosauruses were amazing consumers and had about 60 teeth, and they could consume up to 500 lbs of food at one time. <br>Today, almost everyone knows what a T-rex is and what it looks like.";
 }
 
 function Velociraptor(){
   document.getElementById("heading1").innerHTML = "Velociraptor";
-  document.getElementById("anything").innerHTML = "Other than T-rexes, velociraptors are the most known dinosaurs. They are known to be small and fast, and the word velociraptor is a direct translation to speedy thief. They typically weighed a light 30 lbs at max.";
+  document.getElementById("anything").innerHTML = "Other than T-rexes, velociraptors are the most known dinosaurs. <br>They are known to be small and fast, and the word velociraptor is a direct translation to speedy thief. <br>They typically weighed a light 30 lbs at max.";
   document.getElementById("img").src = "velociraptor.png";
   document.getElementById("img").alt = "velociraptor";
 }
 
 function Pterodactyl(){
   document.getElementById("heading1").innerHTML = "Pterodactyl";
-  document.getElementById("anything").innerHTML = "Pterodactyls were an extremely successful group of reptiles. They flourished all throughout the age of dinosaurs, a period of more than 150 million years. Over time, the earliest pterodactyls — relatively small flying reptiles with sturdy bodies and long tails — evolved into a broad variety of species. Some had long, slender jaws, elaborate head crests, or specialized teeth, and some were extraordinarily large.";
+  document.getElementById("anything").innerHTML = "Pterodactyls were an extremely successful group of reptiles. They flourished all throughout the age of dinosaurs, a period of more than 150 million years. <br>Over time, the earliest pterodactyls — relatively small flying reptiles with sturdy bodies and long tails — evolved into a broad variety of species. <br>Some had long, slender jaws, elaborate head crests, or specialized teeth, and some were extraordinarily large.";
   document.getElementById("img").src = "pterodactyl.png";
   document.getElementById("img").alt = "pterodactyl";
 }
 
 function Spinosaurus(){
   document.getElementById("heading1").innerHTML = "Spinosaurus";
-  document.getElementById("anything").innerHTML = "The Spinosaurus, which was longer and heavier than Tyrannosaurus, is the largest known carnivorous dinosaur. It possessed a long, narrow skull 1.75 meters long, resembling that of a crocodile, and its teeth were straight and conical. In addition, the bones of its skeleton were more compact and dense than those of similar land-dwelling dinosaurs, which allowed it greater control over its buoyancy underwater; this characteristic has led some researchers to argue that Spinosaurus was primarily an aquatic predator rather than a land one.";
+  document.getElementById("anything").innerHTML = "The Spinosaurus, which was longer and heavier than Tyrannosaurus, is the largest known carnivorous dinosaur. <br>It possessed a long, narrow skull 1.75 meters long, resembling that of a crocodile, and its teeth were straight and conical. <br>In addition, the bones of its skeleton were more compact and dense than those of similar land-dwelling dinosaurs, <br>which allowed it greater control over its buoyancy underwater; <br>this characteristic has led some researchers to argue that Spinosaurus was primarily an aquatic predator rather than a land one.";
   document.getElementById("img").src = "spinosaurus.png";
   document.getElementById("img").alt = "spinosaurus";
 }
 
 function Triceratops(){
   document.getElementById("heading1").innerHTML = "Triceratops";
-  document.getElementById("anything").innerHTML = "The Triceratops dinosaur is one of the most famous dinosaurs. It is widely known for its large head with three horns. It is believed that the triceratops lived during a portion of the Cretaceous period around 70 million years ago. Fossils have been found in North America, in both the western US and Canada.";
+  document.getElementById("anything").innerHTML = "The Triceratops dinosaur is one of the most famous dinosaurs. It is widely known for its large head with three horns. <br>It is believed that the triceratops lived during a portion of the Cretaceous period around 70 million years ago. <br>Fossils have been found in North America, in both the western US and Canada.";
   document.getElementById("img").src = "triceratops.png";
   document.getElementById("img").alt = "triceratops";
 }
 
 function Ankylosaurus(){
   document.getElementById("heading1").innerHTML = "Ankylosaurus";
-  document.getElementById("anything").innerHTML = "Often compared to an army tank or bus, Ankylosaurus was a heavily armored dinosaur with a large club-like protrusion at the end of its tail. Ankylosaurus means <b> fused lizard </b> in Greek, and it was given that name because bones in its skull and other parts of its body were fused, making the dinosaur extremely rugged.";
+  document.getElementById("anything").innerHTML = "Often compared to an army tank or bus, Ankylosaurus was a heavily armored dinosaur with a large club-like protrusion at the end of its tail. <br>Ankylosaurus means <b> fused lizard </b> in Greek, and it was given that name because bones in its skull and other parts of its body were fused, <br>making the dinosaur extremely rugged.";
   document.getElementById("img").src = "ankylosaurus.png";
   document.getElementById("img").alt = "ankylosaurus";
 }
 
 function Brontosaurus(){
   document.getElementById("heading1").innerHTML = "Brontosaurus";
-  document.getElementById("anything").innerHTML = "The Brontosaurus is one of the world's most beloved dinosaurs, unusual, perhaps, for a reptile whose validity was doubted for more than 100 years. The Brontosaurus was part of a group of typically large dinosaurs with long necks and long tails. It had a long, thin neck and a small head adapted for a vegetarian lifestyle, a bulky, heavy torso, and a long, whip-like tail. The various species lived during the Late Jurassic epoch and were extinct by the end of the Jurassic.";
+  document.getElementById("anything").innerHTML = "The Brontosaurus is one of the world's most beloved dinosaurs, unusual, perhaps, for a reptile whose validity was doubted for more than 100 years. <br>The Brontosaurus was part of a group of typically large dinosaurs with long necks and long tails. It had a long, <br>thin neck and a small head adapted for a vegetarian lifestyle, a bulky, heavy torso, and a long, whip-like tail. <br>The various species lived during the Late Jurassic epoch and were extinct by the end of the Jurassic.";
   document.getElementById("img").src = "brontosaurus.png";
   document.getElementById("img").alt = "brontosaurus";
 }
 function Ichthyosaur(){
   document.getElementById("heading1").innerHTML = "Ichthyosaur";
-  document.getElementById("anything").innerHTML = "Ichthyosaurs appear to provide an excellent opportunity to test the extent of predictability of evolutionary processes. They descended from land-based reptiles, conquered the open oceans, became agile hunters of fish and squid, and persisted over incredibly long 155 million years. Ichthyosaur are distant relatives of lizards and snakes. They were the most highly specialized aquatic reptiles in the past, but ichthyosaurs were not dinosaurs.";
+  document.getElementById("anything").innerHTML = "Ichthyosaurs appear to provide an excellent opportunity to test the extent of predictability of evolutionary processes. <br>They descended from land-based reptiles, conquered the open oceans, became agile hunters of fish and squid, and persisted over incredibly long 155 million years. <br>Ichthyosaur are distant relatives of lizards and snakes. They were the most highly specialized aquatic reptiles in the past, but ichthyosaurs were not dinosaurs.";
   document.getElementById("img").src = "ichthyosaur.png";
   document.getElementById("img").alt = "ichthyosaur";
 }
